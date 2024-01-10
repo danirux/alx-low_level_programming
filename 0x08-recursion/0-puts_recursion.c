73dcd3d7b727df6d00c873bbf20bc5a2f8275c6b
danirux <daniellarukia2003@gmail.com>
2024-01-10 13:51:41 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string followed by a new line.
- *@s: The string to print
+ * _puts_recursion - Print a string followed by a new line.
+ *@s: The string to print.
  *
- * Return: Nothing
+ * Return: Nothing.
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
